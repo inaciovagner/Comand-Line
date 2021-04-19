0bccd1b97e7b841637b0cfeb13c4efa9e17c0c20
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-19 10:21:37 +0100

    Add project files.

---
+++ Comand-Line/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+namespace Comand_Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                WriteLine("Two arguments required.");
+                return;
+            }
+            {
+                string param1 = args[0];
+                int param2 = ToInt32(args[1]);
+                WriteLine($"String parameter: {param1}");
+                WriteLine($"Integer parameter: {param2}");
+            }
+        }
+    }
+}
